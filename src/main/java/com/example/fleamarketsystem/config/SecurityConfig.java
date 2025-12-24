5d93cf8387b2493a323627053701ada3d7f734a7
hirohiroto112607 <113352696+hirohiroto112607@users.noreply.github.com>
2025-12-24 10:02:09 +0900

    管理者用のテンプレートファイルを削除し、アプリケーションの設定ファイルを追加しました。これにより、管理者機能の表示が改善され、Auth0を使用した認証設定が追加されました。

--- src/main/java/com/example/fleamarketsystem/config/SecurityConfig.java
+++ src/main/java/com/example/fleamarketsystem/config/SecurityConfig.java
@@ -8,10 +8,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		/*
+		 * This is where we configure the security required for our endpoints and setup
+		 * our app to serve as
+		 * an OAuth2 Resource Server, using JWT validation.
+		 */
+		return http
+				.authorizeHttpRequests((authorize) -> authorize
+						.requestMatchers("/api/public").permitAll()
+						.requestMatchers("/api/private").authenticated()
+						.requestMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages"))
+						// ここをいじって認可設定を行う
+				.cors(withDefaults())
+				.oauth2ResourceServer(oauth2 -> oauth2
+						.jwt(withDefaults()))
+				.build();
+	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
